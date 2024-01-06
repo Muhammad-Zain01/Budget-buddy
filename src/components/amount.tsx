d8159dbcfb121053efa4c8ac0db2352d48519113
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-06 17:59:56 +0500

    Update amount.tsx

--- src/components/amount.tsx
+++ src/components/amount.tsx
@@ -2,7 +2,7 @@
 const Amount = () => {
     return (
         <>
-            <span style={{fontSize: 15, fontWeight: 500}}>
+            <span style={{ fontSize: 15, marginRight: 10, fontWeight: 500 }}>
                 $123.00
             </span>
         </>
