ad4eb98dac5fc5b93eb41d665d47cf7f82eee2ec
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-06 18:00:01 +0500

    Update category-tabs.tsx

--- src/components/category-tabs.tsx
+++ src/components/category-tabs.tsx
@@ -1,23 +1,46 @@
 import { CategoryType } from "@/pages/category";
-import { Segmented, Tabs } from "@/ui/imports/ui-import";
+import { Segmented } from "@/ui/imports/ui-import";
 import { Dispatch, SetStateAction } from "react";
+import styled from "styled-components";
+
+const SegmentedWrapper = styled(Segmented)`
+    background-color: ${(props) => props.theme.contentBackground};
+    color: ${(props) => props.theme.color};
+    width: 60%;
+    margin-bottom: 20px;
+    
+    .ant-segmented-item:not(.ant-segmented-item-selected):hover{
+        /* background-color: ${(props) => props.theme.theme === 'dark' && '#394655'} !important; */
+        color: ${(props) => props.theme.theme === 'dark' && 'white'} !important;
+    }
+ 
+    label{
+        margin-right:10px;
+    }
+
+`
+const SegemnetedDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
 type ComponnetProps = {
     changeType: Dispatch<SetStateAction<CategoryType | string>>
 }
 const CategoryTabs: React.FC<ComponnetProps> = ({ changeType }): JSX.Element => {
     return (
-        <div style={{ padding: 20 }}>
-            <Segmented
+        <SegemnetedDiv>
+            <SegmentedWrapper
                 options={[
                     { label: 'Income', value: 'income' },
                     { label: 'Expense', value: 'expense' }
                 ]}
-                defaultValue="expense"
+                defaultValue="income"
                 block
                 onChange={(e: string) => { changeType(e) }}
+                size="large"
             />
-        </div>
+        </SegemnetedDiv>
     )
 }
 
