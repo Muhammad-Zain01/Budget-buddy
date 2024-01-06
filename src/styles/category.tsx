be47695a03b757b93fe254e42da72a8ef63bda5e
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-06 18:00:15 +0500

    Update category.tsx

--- src/styles/category.tsx
+++ src/styles/category.tsx
@@ -4,23 +4,46 @@ import UI_Title from "@/ui/components/ui-title";
 export const CategoryWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: start;
 `
 
 export const CategoryCard = styled(Card)`
-    width: 18%;
-    min-width:140px;
+    width: calc(20% - 10px);
     margin: 5px 5px;
     background-color: ${(props) => props.theme.cardBackground};
     color: ${(props) => props.theme.color};
-    /* box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; */
     border: 1px solid ${props => props.theme.cardBorder};
     padding: 10px 0px;
+
+    &:hover{
+        border: 1px solid #949494;
+    }
     .ant-card-body{
         padding: 15px;
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+
+    @media only screen and (max-width: 1100px) {
+        &{
+            width: calc(25% - 10px);
+        }
+    }
+    @media only screen and (max-width: 783px) {
+        &{
+            width: calc(33.33% - 10px);
+        }
+    }
+    @media only screen and (max-width: 600px) {
+        &{
+            width: calc(50% - 10px);
+        }
+    }
+    @media only screen and (max-width: 450px) {
+        &{
+            width: 100%;
+        }
     }
 `
 
