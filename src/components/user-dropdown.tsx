b14abf3b74b6f8584690dfa3b276a254f5a8e4db
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2024-01-10 17:57:36 +0500

    Create user-dropdown.tsx

---
+++ src/components/user-dropdown.tsx
@@ -0,0 +1,12 @@
+import { Avatar } from "@/ui/imports/ui-import";
+
+
+const UserDropDown: React.FC = (): JSX.Element => {
+    return (
+        <div>
+            <Avatar size={38}>U</Avatar>
+        </div>
+    )
+}
+
+export default UserDropDown;
