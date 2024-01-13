88e7a8448c17ee06b453fa6b10d209dbf5bc34d5
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-13 17:21:19 +0500

    Update user-dropdown.tsx

--- src/components/user-dropdown.tsx
+++ src/components/user-dropdown.tsx
@@ -1,10 +1,26 @@
-import { Avatar } from "@/ui/imports/ui-import";
+import { UserAvatar } from "@/layout/dashboard.style";
+import { MenuProps } from "antd";
+import UI_Dropdown from "@/ui/components/ui-dropdown";
 
 
 const UserDropDown: React.FC = (): JSX.Element => {
+    const UserMenu: MenuProps['items'] = [
+        {
+            key: '1',
+            label: (
+                <p>
+                    asdfasdf
+                </p>
+            ),
+        },
+    ];
     return (
         <div>
-            <Avatar size={38}>U</Avatar>
+            <UI_Dropdown menu={{ items: UserMenu }} trigger={['click']}>
+                <UserAvatar style={{ marginLeft: 50 }} size={42} src={false && "https://api.dicebear.com/7.x/miniavs/svg?seed=30"}>
+                    ZM
+                </UserAvatar>
+            </UI_Dropdown>
         </div>
     )
 }
