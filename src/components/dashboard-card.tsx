96f7811e792b1fbf9d82bba869b4c61d3fc6187b
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-14 14:17:56 +0500

    Create dashboard-card.tsx

---
+++ src/components/dashboard-card.tsx
@@ -0,0 +1,29 @@
+import { DashboardCard as Card, DIconWraper, DashboardCardWrapper, DashboardCardlabel, IconDiv, WalletIcon } from "@/styles/dashboard";
+import Amount from "./amount";
+import UI_Title from "@/ui/components/ui-title";
+import React from "react";
+
+type ComponentProps = {
+    label: string;
+    amount: string;
+    icon: React.ReactNode;
+}
+const DashboardCard: React.FC<ComponentProps> = ({ label, amount, icon }): JSX.Element => {
+    return (
+        <Card>
+            <DashboardCardWrapper>
+                <DIconWraper>
+                    <IconDiv>
+                        {icon}
+                    </IconDiv>
+                </DIconWraper>
+                <div>
+                    <Amount amount={amount} weight={600} size={23} />
+                    <DashboardCardlabel>{label}</DashboardCardlabel>
+                </div>
+            </DashboardCardWrapper>
+        </Card>
+    )
+}
+
+export default DashboardCard;
