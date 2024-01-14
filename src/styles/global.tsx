f822948e35872118a25ceda5c1cba118070fbdcd
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-14 14:18:54 +0500

    Update global.tsx

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
-export const AmountSpan = styled.span`
-    font-size: 15px;
-    margin-right: 10px;
-    font-weight: 500;
-`
+// export const AmountSpan = styled.span`
+//     margin-right: 10px;
+// `
