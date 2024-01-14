6e18359ed6d42521bbfb93773ece6339ab2d6694
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-14 14:18:43 +0500

    Create index.tsx

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const Settings: NextPage = () : JSX.Element => {
+    return (
+        <div>
+            Settings Page
+        </div>
+
+    )
+}
+
+export default Settings;
