38ddc15fbf4f108c0510167a527ceeaff531953c
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-14 14:18:40 +0500

    Create index.tsx

---
+++ src/pages/insights/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Insights: NextPage = (): JSX.Element => {
+    return (
+        <div>
+            Insidsf
+        </div>
+    )
+}
+
+export default Insights;
