704dd087ff745e731cd2c38ad87b8c6b1341f5f2
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-14 14:17:16 +0500

    Update category-card.tsx

--- src/components/category-card.tsx
+++ src/components/category-card.tsx
@@ -22,14 +22,13 @@ const CategoryCard: React.FC<ComponentProps> = ({ category }): JSX.Element => {
     }
     ];
     return (
-        <Card style={{ position: 'relative' }}>
+        <Card>
             <CategoryMenu>
                 <DropMenu items={CategoryMenuItems} />
             </CategoryMenu>
             <Icon icon={icon} size={50} />
             <CategoryHeading level={5} style={{}}>{title}</CategoryHeading>
-            <Amount amount={'123'} />
-
+            {/* <Amount amount={'123'} /> */}
             {/* <UI_Switch size="small" defaultChecked={isChecked} /> */}
         </Card>
     )
