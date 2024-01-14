55e60bb9bd8a44934d08407d28fcd994404023f9
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-14 14:18:34 +0500

    Update index.tsx

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,18 @@
+import DashboardCard from "@/components/dashboard-card";
+import { Expense, Income, Savings, Wallet } from "@/components/icon";
+import { DashboardCardsWrapper } from "@/styles/dashboard";
+import { NextPage } from "next";
 
-export default function Dashboard() {
+const Dashboard: NextPage = (): JSX.Element => {
     return (
         <>
-            Dashboard
+            <DashboardCardsWrapper>
+                <DashboardCard label="Balance" amount="2190.90" icon={<Wallet color='white' size={25} />} />
+                <DashboardCard label="Income" amount="21.30" icon={<Income color='white' size={25} />} />
+                <DashboardCard label="Savings" amount="1875.20" icon={<Savings color='white' size={25} />} />
+                <DashboardCard label="Expenses" amount="19.112" icon={<Expense color='white' size={25} />} />
+            </DashboardCardsWrapper>
         </>
     )
 }
+export default Dashboard
