3290fe7259cb0616dbc09fc1135927d3548174cd
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-20 19:08:44 +0500

    Create auth.tsx

---
+++ src/styles/auth.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+export const AuthWrapper = styled.div`
+    display: flex; 
+    justify-content: center;
+    height: 98vh;
+`
+export const AuthContainer = styled.div`
+    display: flex; 
+    width: 80%;
+    margin: 70px 0px;
+    background: white;
+`
+
+export const AuthBox = styled.div`
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const AuthFormWrapper = styled.div`
+    width: 80%
+`
