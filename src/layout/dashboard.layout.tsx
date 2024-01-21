21a0265792dad4b51a7429d2eab2b1438b6b8e24
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-21 19:52:05 +0500

    Update dashboard.layout.tsx

--- src/layout/dashboard.layout.tsx
+++ src/layout/dashboard.layout.tsx
@@ -12,6 +12,7 @@ import useLoadingState from '@/hooks/useLoadingState';
 import UserDropDown from '@/components/user-dropdown';
 import Timeline from '@/components/timeline';
 import ThemeShifter from '@/components/theme-shifter';
+import { signOut, useSession } from 'next-auth/react';
 
 const HeaderItems = [
     {
@@ -59,46 +60,55 @@ const HeaderItems = [
 ]
 
 const DasboardLayout: React.FC<ChildNodeType> = ({ children }) => {
+    const session = useSession();
+    const status = session?.status
+    console.log(session);
+    console.log(session?.status);
     const path = usePathname().split('/')[1];
     const { sidebar, toggleSidebar } = useLayout()
     const { loading } = useLoadingState();
     const [pageKey, setKey] = useState(path)
-
     const title = HeaderItems.find(item => pageKey === item.key)?.title
+    // signOut()
     return (
-        <DashboardLayoutWrapper>
-            <DashboardSidebar width={240} collapsed={sidebar}>
-                <DashboardMenu
-                    onClick={(e) => { setKey(e.key) }}
-                    defaultSelectedKeys={[path]}
-                    items={HeaderItems}
-                />
-                <UI_Button onClick={toggleSidebar}>Toggle Sidebar</UI_Button>
-            </DashboardSidebar>
-            <Layout>
-                <DashboardHeader>
-                    <DashboardHeaderHeading level={3}>{title}</DashboardHeaderHeading>
-                    <HeaderItemsWraper>
-                        <Timeline />
-                        <ThemeShifter />
-                        <UserDropDown />
-                    </HeaderItemsWraper>
-                </DashboardHeader>
+        <>
+            {
+                status === 'authenticated' &&
+                <DashboardLayoutWrapper>
+                    <DashboardSidebar width={240} collapsed={sidebar}>
+                        <DashboardMenu
+                            onClick={(e) => { setKey(e.key) }}
+                            defaultSelectedKeys={[path]}
+                            items={HeaderItems}
+                        />
+                        <UI_Button onClick={toggleSidebar}>Toggle Sidebar</UI_Button>
+                    </DashboardSidebar>
+                    <Layout>
+                        <DashboardHeader>
+                            <DashboardHeaderHeading level={3}>{title}</DashboardHeaderHeading>
+                            <HeaderItemsWraper>
+                                <Timeline />
+                                <ThemeShifter />
+                                <UserDropDown />
+                            </HeaderItemsWraper>
+                        </DashboardHeader>
 
-                <DashboardContent>
-                    {loading ? <Loader /> : <>{children}</>}
-                </DashboardContent>
-                <DashboardFooter>
-                    All Right Reservered By Muhammad Zain  &nbsp;
-                    <Link href="https://github.com/Muhammad-Zain01">
-                        <Github size={17} />
-                    </Link>&nbsp;&nbsp;
-                    <Link href="https://www.linkedin.com/in/muhammad-zain01/">
-                        <LinkedIn size={17} />
-                    </Link>
-                </DashboardFooter>
-            </Layout>
-        </DashboardLayoutWrapper>)
+                        <DashboardContent>
+                            {loading ? <Loader /> : <>{children}</>}
+                        </DashboardContent>
+                        <DashboardFooter>
+                            All Right Reservered By Muhammad Zain  &nbsp;
+                            <Link href="https://github.com/Muhammad-Zain01">
+                                <Github size={17} />
+                            </Link>&nbsp;&nbsp;
+                            <Link href="https://www.linkedin.com/in/muhammad-zain01/">
+                                <LinkedIn size={17} />
+                            </Link>
+                        </DashboardFooter>
+                    </Layout>
+                </DashboardLayoutWrapper>}
+        </>
+    )
 };
 
 export default DasboardLayout;
