ffae33bd4aacd931396449aa2ae6f9d5eeebe421
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-27 17:16:58 +0500

    Update global.tsx

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,6 +1,7 @@
 import styled, { createGlobalStyle } from "styled-components";
 import Link from "next/link";
 import { SelectBoxOptions } from "@/ui/components/ui-select";
+import UI_Button from "@/ui/components/ui-button";
 
 export const DashboardLink = styled(Link)`
     transition: none !important;
@@ -12,3 +13,11 @@ export const GlobalStyle = createGlobalStyle`
     ${SelectBoxOptions}
 `;
 
+
+export const AddButton = styled(UI_Button)`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    font-weight: 500;
+    margin-bottom: 8px;
+`
