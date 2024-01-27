974bb0fd966466c901e985504fa43487e788faed
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-27 17:16:20 +0500

    Update category-tabs.tsx

--- src/components/category-tabs.tsx
+++ src/components/category-tabs.tsx
@@ -1,46 +1,28 @@
 import { CategoryType } from "@/pages/category";
-import { Segmented } from "@/ui/imports/ui-import";
+import UI_Tabs from "@/ui/components/ui-tabs";
+import { TabsProps } from "antd";
 import { Dispatch, SetStateAction } from "react";
-import styled from "styled-components";
-
-const SegmentedWrapper = styled(Segmented)`
-    background-color: ${(props) => props.theme.contentBackground};
-    color: ${(props) => props.theme.color};
-    width: 60%;
-    margin-bottom: 20px;
-    
-    .ant-segmented-item:not(.ant-segmented-item-selected):hover{
-        /* background-color: ${(props) => props.theme.theme === 'dark' && '#394655'} !important; */
-        color: ${(props) => props.theme.theme === 'dark' && 'white'} !important;
-    }
- 
-    label{
-        margin-right:10px;
-    }
-
-`
-const SegemnetedDiv = styled.div`
-    display: flex;
-    justify-content: center;
-`
 
 type ComponnetProps = {
-    changeType: Dispatch<SetStateAction<CategoryType>>
+    changeType: Dispatch<SetStateAction<CategoryType>>,
+    addButton: React.ReactNode
 }
-const CategoryTabs: React.FC<ComponnetProps> = ({ changeType }): JSX.Element => {
+
+const CategoryTabs: React.FC<ComponnetProps> = ({ changeType, addButton }): JSX.Element => {
+    const items: TabsProps['items'] = [
+        {
+            key: 'income',
+            label: 'Income',
+        },
+        {
+            key: 'expense',
+            label: 'Expense',
+        },
+    ];
+    const onChange = (e) => { changeType(e) }
     return (
-        <SegemnetedDiv>
-            <SegmentedWrapper
-                options={[
-                    { label: 'Income', value: 'income' },
-                    { label: 'Expense', value: 'expense' }
-                ]}
-                defaultValue="income"
-                block
-                onChange={(e: string) => { changeType(e) }}
-                size="large"
-            />
-        </SegemnetedDiv>
+        <UI_Tabs tabBarExtraContent={addButton} items={items} onChange={onChange} />
+
     )
 }
 
