49560faa1df6e2b58a6f2cdb5399123c99b73f35
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-27 17:16:39 +0500

    Update index.tsx

--- src/pages/accounts/index.tsx
+++ src/pages/accounts/index.tsx
@@ -1,15 +1,24 @@
 import AccountCard from "@/components/account-card";
+import AddAccountModal from "@/components/add-account-modal";
+import { Add } from "@/components/icon";
 import { AccountWrapper } from "@/styles/account";
+import { AddButton } from "@/styles/global";
+import { useState } from "react";
 export default function Accounts() {
+    const [isOpen, setIsOpen] = useState(false);
     const data = [
         { icon: 'bank', title: "Bank", isChecked: true, amount: '200' },
         { icon: 'bank', title: "Bank", isChecked: true, amount: '500' },
     ]
     return (
         <>
+            <div style={{ display: 'flex', justifyContent: 'end' }}>
+                <AddButton type='primary' icon={<Add size={18} />} onClick={() => setIsOpen(true)}>Add Account</AddButton>
+            </div>
             <AccountWrapper>
                 {data.map((item, index) => <AccountCard key={index} data={item} />)}
             </AccountWrapper>
+            <AddAccountModal open={isOpen} setOpen={setIsOpen} />
         </>
     )
 }
