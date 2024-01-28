1666332146dee21473c3905c362a509ec9faba15
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-01-28 16:07:32 +0500

    Update add-transaction-modal.tsx

--- src/components/add-transaction-modal.tsx
+++ src/components/add-transaction-modal.tsx
@@ -48,7 +48,7 @@ const AddTransactionModal: React.FC<ComponentProps> = ({ open, setOpen }) => {
                     <div style={{ marginTop: 20, display: 'flex', justifyContent: 'center' }}>
                         {
                             TransactionType.map(item =>
-                                <AccountIconDiv key={item[0]} isSelected={transactionType == item[0] ? true : false} onClick={() => nextClick(item)}>
+                                <AccountIconDiv key={item[0]} $isSelected={transactionType == item[0] ? true : false} onClick={() => nextClick(item)}>
                                     <Icon icon={item[0]} size={50} />
                                     <UI_Text style={{ fontSize: 13, marginTop: 5 }}>{item[1]}</UI_Text>
                                 </AccountIconDiv>
