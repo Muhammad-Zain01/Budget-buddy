b03b207f92f326e0f7381c79e94a2a1ad9d59994
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-02-04 20:24:12 +0500

    Update add-category-modal.tsx

--- src/components/category/add-category-modal.tsx
+++ src/components/category/add-category-modal.tsx
@@ -1,34 +1,51 @@
 import UI_Modal from "@/components/ui/ui-modal";
 import UI_Select from "@/components/ui/ui-select";
 import IconsDiv from "../common/icons-div";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import UI_Input from "@/components/ui/ui-input";
 import UI_Button from "@/components/ui/ui-button";
 import { FormItemContainer, FormItemLabel } from "@/styles/global";
 import useFormState from "@/hooks/useFormState";
 import { CategoryType } from "@/pages/categories";
+import CategoryModalContainer from "./category-modal-container";
+import useMessage from "@/hooks/useMessage";
+import { addCategory } from "@/api/request";
+import { ResponseErrors } from "@/common/errors";
+import { useRouter } from "next/router";
 
 type ComponentProps = {
     open: boolean;
     setOpen: (value: boolean) => void;
     defaultCategory: CategoryType;
 }
-type FormState = {
+export type FormState = {
     icon: string;
     categoryType: CategoryType;
     categoryName: string;
+    setCategories: (value: any) => void;
 }
-const AddCategoryModal: React.FC<ComponentProps> = ({ open, setOpen, defaultCategory }) => {
+const AddCategoryModal: React.FC<ComponentProps> = ({ open, setOpen, defaultCategory, setCategories }) => {
     const InitialState = { icon: "", categoryType: defaultCategory, categoryName: "" }
-    const { form, reset, setValue } = useFormState<FormState>(InitialState)
-    const { icon, categoryType, categoryName } = form;
-
-    const Options: { value: CategoryType, label: string }[] = [
-        { value: 'income', label: 'Income' },
-        { value: 'expense', label: 'Expense' }
-    ]
-    const onSave = () => {
-        console.log("SAVING");
+    const rules = { required: ['categoryName'] };
+    const { form, reset, check, setValue, formRules } = useFormState<FormState>(InitialState, rules)
+    const [loading, setLoading] = useState(false);
+    const message = useMessage();
+    const onSave = async () => {
+        if (!check()) return;
+        if (form?.icon == '') {
+            return message.error('Please select icon');
+        }
+        setLoading(true)
+        const response = await addCategory(form)
+        if (response?.status) {
+            setCategories(prev => [...prev, response?.data])
+            message.success('Successfully added category');
+            setOpen(false);
+        }
+        else {
+            message.error(response?.message ? ResponseErrors[response?.message] : "Something went wrong")
+        }
+        setLoading(false)
     }
     const onCancel = () => {
         reset();
@@ -41,35 +58,10 @@ const AddCategoryModal: React.FC<ComponentProps> = ({ open, setOpen, defaultCate
             onCancel={onCancel}
             centered
             footer={
-                <UI_Button type="primary" onClick={onSave}>Save</UI_Button>
+                <UI_Button loading={loading} type="primary" onClick={onSave}>Save</UI_Button>
             }
         >
-            <FormItemContainer>
-                <FormItemLabel>Category Name</FormItemLabel>
-                <UI_Input
-                    value={categoryName}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => { setValue<string>("categoryName", e.currentTarget.value) }}
-                    placeholder="Enter Category Name..."
-                />
-            </FormItemContainer>
-            <FormItemContainer>
-                <FormItemLabel>Category Type</FormItemLabel>
-                <UI_Select
-                    value={categoryType}
-                    options={Options}
-                    onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                        const value = e as unknown;
-                        setValue<CategoryType>("categoryType", value as CategoryType)
-                    }}
-                />
-            </FormItemContainer>
-            <FormItemContainer>
-                <FormItemLabel>Select Icon</FormItemLabel>
-                <IconsDiv
-                    selectIcon={(value) => setValue('icon', value)}
-                    selectedIcon={icon}
-                />
-            </FormItemContainer>
+            <CategoryModalContainer form={form} setValue={setValue} rules={formRules} />
         </UI_Modal>
     )
 }
