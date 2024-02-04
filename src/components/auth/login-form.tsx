43f7da86935f0c5751c41e2ea0839418a3cb55e6
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-02-04 20:24:06 +0500

    Update login-form.tsx

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -4,18 +4,29 @@ import Link from "next/link"
 import { Email, Password } from "../icon"
 import { useState } from "react"
 import { signIn } from "next-auth/react"
+import useFormState from "@/hooks/useFormState"
+import useMessage from "@/hooks/useMessage"
+import { ResponseErrors } from "@/common/errors"
+import { useRouter } from "next/router"
+import { GiConsoleController } from "react-icons/gi"
 
+type FormState = {
+    username: string;
+    password: string;
+}
 export const LoginForm: React.FC = (): JSX.Element => {
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const initialState = { username: '', password: '' }
+    const rules = { required: ['username', 'password'] };
+    const { form, setValue, check, formRules } = useFormState<FormState>(initialState, rules)
+    const { username, password } = form;
+    const [loading, setLoading] = useState(false);
+    const message = useMessage()
+    const router = useRouter();
 
-    const resetValues = () => {
-        setUsername('');
-        setPassword('');
-    }
-
+    const onEnterClick = (e) => e.keyCode == 13 && onSubmit();
     const onSubmit = async () => {
-        // VALIDATIONS
+        if (!check()) return;
+        setLoading(true);
         const response = await signIn(
             'credentials',
             {
@@ -23,6 +34,16 @@ export const LoginForm: React.FC = (): JSX.Element => {
                 password: password,
                 redirect: false
             } as { username: string, password: string, redirect: boolean })
+
+        if (response?.ok) {
+            message.success("you have been signed in successfully")
+            router.push('/dashboard');
+        } else {
+            if (response?.error) {
+                message.error(ResponseErrors[response?.error])
+            }
+        }
+        setLoading(false);
     }
     return (
         <AuthBox>
@@ -33,13 +54,13 @@ export const LoginForm: React.FC = (): JSX.Element => {
                 </AuthHeadingWrapper>
                 <FieldsWrapper>
                     <AuthLabel>Email or Username</AuthLabel>
-                    <UI_Input prefix={<Email color="#bcbcbc" size={18} right={5} />} placeholder="Enter Your Email or Username" onChange={(e) => setUsername(e?.target?.value)} />
+                    <UI_Input {...formRules.username} prefix={<Email color="#bcbcbc" size={18} right={5} />} placeholder="Enter Your Email or Username" onChange={(e) => setValue('username', e?.target?.value)} />
                 </FieldsWrapper>
                 <FieldsWrapper >
                     <AuthLabel>Password</AuthLabel>
-                    <UI_Input prefix={<Password color="#bcbcbc" size={18} right={5} />} placeholder="Enter Your Password" type="password" onChange={(e) => setPassword(e?.target?.value)} />
+                    <UI_Input {...formRules.password} prefix={<Password color="#bcbcbc" size={18} right={5} />} placeholder="Enter Your Password" type="password" onChange={(e) => setValue('password', e?.target?.value)} onKeyDown={onEnterClick} />
                 </FieldsWrapper>
-                <AuthButton onClick={onSubmit}>
+                <AuthButton loading={loading} onClick={onSubmit}>
                     Login
                 </AuthButton>
                 <AuthChanger>
