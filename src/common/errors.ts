784143c493617f2082a4de48ecb8652e31979340
Muhammad-Zain01 <zainmemon010@gmail.com>
2024-02-04 20:23:59 +0500

    Create errors.ts

---
+++ src/common/errors.ts
@@ -0,0 +1,6 @@
+export const ResponseErrors = {
+    "WRONG_PASSWORD": 'Your Entered Password is Wrong',
+    "NOT_FOUND": 'User not found',
+    "MISSING_FIELDS": 'All Fields need to be provided',
+    "UN_AUTHORIZED": 'you are not authorized to do this action',
+}
